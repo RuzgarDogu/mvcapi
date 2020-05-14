45fd86fb331866b175bd17f0a489ac7107bbf262
Rüzgar Doğu <doguruzgar@gmail.com>
2020-05-14 21:05:01 +0300

    Update config.php

--- config.php
+++ config.php
@@ -12,19 +12,11 @@ const LOCAL_DB_USER = 'root';
 const LOCAL_DB_PASS = '';
 const LOCAL_DB_PORT = '3306';
 
-const SQL_DB_TYPE = 'sqlsrv';
-const SQL_DB_HOST = '82.222.176.46';
-const SQL_DB_NAME = 'MOBILEDB';
-const SQL_DB_USER = 'emre';
-const SQL_DB_PASS = 'a14511e285d3284772e32f0a458f16b7c2f65eab2bdc925c8695eef09e3b1e35';
-const SQL_DB_PORT = '33081';
-
 // Her bir aplikasyon için farklı bir signature oluşturmak lazım olabilir.
-const JWT_SECRET = '317d9e66ce8ece72';
-const HASH_PASSWORD_KEY = 'Wr15oR56';
-const HASH_GENERAL_KEY = '11qqaaZZ';
+const JWT_SECRET = 'WriteYourJwtSecretHere';
+const HASH_PASSWORD_KEY = 'HashPass';
+const HASH_GENERAL_KEY = 'HashGeneral';
 
-// const IZINLIIP = "176.40.252.135";
 const IZINLIIP = "127.0.0.1";
 // const CONTENTTYPE = 'text/plain';
 const CONTENTTYPE = 'application/json';
